bfa445b1079ee01c75510848df9c6e482a3755b1
Selçuk Öztürk <selcukahmed@gmail.com>
2021-01-19 09:52:56 +0300

    Removed Leiningen path

--- custom.el
+++ custom.el
@@ -43,6 +43,6 @@
   )
 
 (use-package! all-the-icons)
-(add-to-list 'exec-path "/home/selcuk/.sdkman/candidates/leiningen/current/bin")
+;; (add-to-list 'exec-path "/home/selcuk/.sdkman/candidates/leiningen/current/bin")
 
 (load-theme 'material t)
